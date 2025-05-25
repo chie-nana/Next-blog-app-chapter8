e2521d3e4e65df3fd885202d939d66ac616684ed
Chie Nakagawa <chie.design22@gmail.com>
2025-05-25 15:48:38 +0900

    microCms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,31 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { Post } from "@/app/_types/Post";
+import { MicroCmsPost } from "@/app/_types/MicroCmsPost";
 import classes from "@/app/_styles/Home.module.css"
 
 const Home = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [posts, setPosts] = useState<MicroCmsPost[]>([])
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetcher = async () => {
       try {
-        const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts");
-        if (!res.ok) throw new Error("記事の取得に失敗しました")
-        const data = await res.json();
-        setPosts(data.posts)
+        const res = await fetch('<https://4zmimxorag.microcms.io/api/v1/posts>', {　// 管理画面で取得したエンドポイントを入力してください。
+          headers: {// fetch関数の第二引数にheadersを設定でき、その中にAPIキーを設定します。
+            'X-MICROCMS-API-KEY': 'M8Can9QIcdNtBha8ctgylpLYw7stIF9yrxtB', // 管理画面で取得したAPIキーを入力してください。
+          },
+        })
+        const { contents } = await res.json()
+        setPosts(contents)
       } catch (error: any) {
         setError(error.message);
       } finally {
         setLoading(false);
       }
     };
-    fetcher();
+    fetcher()
   }, []);
   if (loading) {
     return <p>読み込み中</p>;
@@ -50,8 +53,8 @@ const Home = () => {
                 <div>
                   {post.categories.map((category) => {
                     return (
-                      <span key={category} className={classes.postCategory}>
-                        {category}
+                      <span key={category.id} className={classes.postCategory}>
+                        {category.name}
                       </span>
                     );
                   })}
