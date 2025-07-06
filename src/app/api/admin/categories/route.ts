8d13bdd4b80bcd9ab86b6ab524c17298460c749f
Chie Nakagawa <chie.design22@gmail.com>
2025-07-07 06:12:11 +0900

    管理者_カテゴリー削除

--- src/app/api/admin/categories/route.ts
+++ src/app/api/admin/categories/route.ts
@@ -72,3 +72,21 @@ export const PUT = async (
       return NextResponse.json({ status: error.message }, { status: 400 })
   }
 }
+
+export const DELETE = async (
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) => {
+  const { id } = params;
+  try {
+    await prisma.category.delete({
+      where: {
+        id: parseInt(id),
+      },
+    })
+    return NextResponse.json({ status: "OK" }, { status: 200 })
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 })
+  }
+}
