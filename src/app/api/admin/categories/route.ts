e26dba3678e9e2e284619189c388d733ec23cea4
Chie Nakagawa <chie.design22@gmail.com>
2025-07-07 04:49:42 +0900

    管理者_カテゴリ新規作成

--- src/app/api/admin/categories/route.ts
+++ src/app/api/admin/categories/route.ts
@@ -16,3 +16,31 @@ export const GET = async (request: NextRequest) => {
       return NextResponse.json({ status: error.message }, { status: 400 })
   }
 }
+
+interface CreateCategoryRequestBody {
+  name: string;
+}
+
+// POSTという命名にすることで、POSTリクエストの時にこの関数が呼ばれる
+export const POST = async (request: NextRequest, context: any) => {
+  try {
+    // リクエストのbodyを取得
+    const body = await request.json()
+    // bodyの中からnameを取り出す
+    const { name }: CreateCategoryRequestBody = body;
+    // カテゴリーをDBに生成
+    const data = await prisma.category.create({
+      data: {
+        name,
+      },
+    })
+    return NextResponse.json({
+      status: "OK",
+      message: "カテゴリーを作成しました",
+      id: data.id,
+    })
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({status: error.message}, { status: 400 })
+  }
+}
