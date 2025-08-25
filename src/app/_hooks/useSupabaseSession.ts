99adbd510513f41619d53c51e913e0d31bf8ceb2
Chie Nakagawa <chie.design22@gmail.com>
2025-08-25 15:29:01 +0900

    ヘッダーコンポーネント修正

--- src/app/_hooks/useSupabaseSession.ts
+++ src/app/_hooks/useSupabaseSession.ts
@@ -2,7 +2,7 @@ import { supabase } from "@/utils/supabase";
 import { Session } from "@supabase/supabase-js";
 import { useState, useEffect } from "react";
 
-export const useSpabaseSession = () => {
+export const useSupabaseSession = () => {
   // undefind: ログイン状態ロード中, null: ログインしていない, Session: ログインしている
   const [session, setSession] = useState<Session | null | undefined>(undefined);
   const [token, setToken] = useState<string | null>(null);
